ffcfb7c20c830a2898f08d753a7c1c9e9e1901a6
Jeyanth Kallis Sweeton <sweeton.2001@outlook.com>
2023-09-26 15:51:00 +0530

    Globbing And Export Command

---
+++ Module 3/Globbing And Export Command/Answer.sh
@@ -0,0 +1,25 @@
+# 					Spoken Tutorials - Bash Assignment 
+# 				Funded by the National Mission on Education through ICT
+# 				       Indian Institute of Technology Bombay,
+# 					       http://spoken-tutorial.org
+# -------------------------------------------------------------------------------------------------------------------
+# Globbing and Export statement
+
+# 1) Write a Bash script to do all operations discussed under Globbing
+
+    #!/bin/bash
+
+    user=Sweeton
+    echo "$user"
+
+    /bin/bash
+    echo "$user"
+
+    exit
+
+    export user1=Anto
+    echo "$user1"
+
+    /bin/bash
+    echo "$user1"
+
